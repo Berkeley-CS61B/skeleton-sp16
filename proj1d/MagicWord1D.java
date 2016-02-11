3b419fbd26aa30c0e69884a652570705e996d3c9
Josh Hug <hug@cs.berkeley.edu>
2016-02-11 10:35:39 -0800

    added proj1d survey directions

---
+++ proj1d/MagicWord1D.java
@@ -0,0 +1,8 @@
+public class MagicWord1D {
+	public static String magicWord() {
+		/* replace magicWord with the magic word given to you when
+		 * you finished the survey. */
+		String magicWord = "";
+		return magicWord;
+	}
+} 
