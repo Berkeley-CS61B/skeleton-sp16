7aa3b493ef62d5be80f04713a4bfc0ecd5cc18f4
Josh Hug <hug@cs.berkeley.edu>
2016-02-17 13:57:00 -0800

    fixed magicword5.java

---
+++ lab5/MagicWord5.java
@@ -0,0 +1,8 @@
+public class MagicWord5 {
+	public static String magicWord() {
+		/* Replace magicWord with the magic word given in lab.
+		 * If you are submitting early, just put in "early" */
+		String magicWord = "";
+		return magicWord;
+	}
+} 
