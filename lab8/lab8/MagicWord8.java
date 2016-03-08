f30458ed11f0f0951a128c8ffe4e040dc53a43b8
Josh hug <hug@cs.berkeley.edu>
2016-03-08 11:23:16 -0800

    lab8 release

---
+++ lab8/lab8/MagicWord8.java
@@ -0,0 +1,10 @@
+package lab8;
+
+public class MagicWord1D {
+    public static String magicWord() {
+        /* replace magicWord with the magic word or use "early"
+        *  if you are submitting early */
+        String magicWord = "";
+        return magicWord;
+    }
+} 
