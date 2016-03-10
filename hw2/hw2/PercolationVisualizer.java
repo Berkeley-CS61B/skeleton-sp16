ff7878a69d1634aaba1bca7d327cab065fee285e
Josh hug <hug@cs.berkeley.edu>
2016-03-10 12:15:31 -0800

    fixed visualizer bug

--- hw2/hw2/PercolationVisualizer.java
+++ hw2/hw2/PercolationVisualizer.java
@@ -1,75 +1,88 @@
 /******************************************************************************
- *
  *  Compilation:  javac PercolationVisualizer.java
  *  Execution:    java PercolationVisualizer input.txt
  *  Dependencies: Percolation.java
  *
+ *  This program takes the name of a file as a command-line argument.
+ *  From that file, it
+ *
+ *    - Reads the grid size N of the percolation system.
+ *    - Creates an N-by-N grid of sites (intially all blocked)
+ *    - Reads in a sequence of sites (row i, column j) to open.
+ *
+ *  After each site is opened, it draws full sites in light blue,
+ *  open sites (that aren't full) in white, and blocked sites in black,
+ *  with with site (0, 0) in the upper left-hand corner.
+ *
  ******************************************************************************/
 package hw2;                       
-import edu.princeton.cs.introcs.In;
-import edu.princeton.cs.introcs.StdDraw;
+import java.awt.Font;
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
 
 public class PercolationVisualizer {
+    /* UNCOMMENT THE CODE IN ORDER TO RUN.
+    
+    // delay in miliseconds (controls animation speed)
+    private static final int DELAY = 100;
 
-/*    public static void main(String[] args) {
-        // input file
-        In in = new In(args[0]);
-
-        // N-by-N lattice
-        int N = in.readInt();
-
-        // number of distinct sites opened
-        int opened = 0;
-
-        // set x- and y-scale
-        StdDraw.setXscale(0, N);
-        StdDraw.setYscale(0, N);
-
-        // turn on animation mode
-        StdDraw.show(0);
-
+    // draw N-by-N percolation system
+    public static void draw(Percolation perc, int N) {
         StdDraw.clear();
         StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.setXscale(-.05 * N, 1.05 * N);
+        StdDraw.setYscale(-.05 * N, 1.05 * N);   // leave a border to write text
         StdDraw.filledSquare(N / 2.0, N / 2.0, N / 2.0);
 
-        // repeatedly read in sites to open and draw resulting system
+        // draw N-by-N grid
+        for (int row = 0; row < N; row++) {
+            for (int col = 0; col < N; col++) {
+                if (perc.isFull(row, col)) {
+                    StdDraw.setPenColor(StdDraw.BOOK_LIGHT_BLUE);
+                } else if (perc.isOpen(row, col)) {
+                    StdDraw.setPenColor(StdDraw.WHITE);
+                } else {
+                    StdDraw.setPenColor(StdDraw.BLACK);
+                }
+                StdDraw.filledSquare(col + 0.5, N - row - 0.5, 0.45);
+            }
+        }
+
+        // write status text
+        StdDraw.setFont(new Font("SansSerif", Font.PLAIN, 12));
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.text(.25 * N, -N * .025, perc.numberOfOpenSites() + " open sites");
+        if (perc.percolates()) {
+            StdDraw.text(.75 * N, -N * .025, "percolates");
+        } else {
+            StdDraw.text(.75 * N, -N * .025, "does not percolate");
+        }                 
+
+    }
+
+    private static void simulateFromFile(String filename) {
+        In in = new In(filename);
+        int N = in.readInt();
         Percolation perc = new Percolation(N);
+
+        // turn on animation mode
+        StdDraw.show(0); 
+
+        // repeatedly read in sites to open and draw resulting system
+        draw(perc, N);
+        StdDraw.show(DELAY);
         while (!in.isEmpty()) {
             int i = in.readInt();
             int j = in.readInt();
-            if (!perc.isOpen(i, j)) {
-                perc.open(i, j);
-                opened++;
-            }
-
-            // draw percolation system
-            StdDraw.clear();
-            StdDraw.setPenColor(StdDraw.BLACK);
-            StdDraw.filledSquare(N / 2.0, N / 2.0, N / 2.0);
-            for (int row = 0; row < N; row++) {
-                for (int col = 0; col < N; col++) {
-                    if (perc.isFull(row, col)) {
-                        StdDraw.setPenColor(StdDraw.BOOK_LIGHT_BLUE);
-                    } else if (perc.isOpen(row, col)) {
-                        StdDraw.setPenColor(StdDraw.WHITE);
-                    } else {
-                        StdDraw.setPenColor(StdDraw.BLACK);
-                    }
-                    StdDraw.filledSquare(col - 0.5, N - row + 0.5, 0.45);
-                }
-            }
-
-            // write status text
-            StdDraw.setPenColor(StdDraw.BLACK);
-            StdDraw.text(.25 * N, -N * .025, "sites opened = " + opened);
-            if (perc.percolates()) {
-                StdDraw.text(.75 * N, -N * .025, "percolates");
-            } else {
-                StdDraw.text(.75 * N, -N * .025, "does not percolate");
-            }                  
-
-            StdDraw.show(10);
+            perc.open(i, j);
+            draw(perc, N);
+            StdDraw.show(DELAY);
         }
+    }
+
+    public static void main(String[] args) {
+        String filename = args[0];
+        simulateFromFile(filename);
     }*/
 }
