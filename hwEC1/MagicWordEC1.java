372621c575ad19a135593874f4cb1227d5e9b2b5
Josh Hug <hug@cs.berkeley.edu>
2016-03-18 23:32:42 -0700

    spring break extra credit survey

---
+++ hwEC1/MagicWordEC1.java
@@ -0,0 +1,8 @@
+public class MagicWordEC1 {
+	public static String magicWord() {
+		/* replace magicWord with the magic word given to you when
+		 * you finished the survey. */
+		String magicWord = "";
+		return magicWord;
+	}
+} 
