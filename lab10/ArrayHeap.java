59216e553d9b274f8f630852382272cc2ff7a26e
Josh Hug <hug@cs.berkeley.edu>
2016-03-27 22:56:15 -0700

    lab10 alpha files

---
+++ lab10/ArrayHeap.java
@@ -0,0 +1,209 @@
+import java.util.ArrayList;
+
+/**
+ * A Generic heap class. Unlike Java's priority queue, this heap doesn't just
+ * store Comparable objects. Instead, it can store any type of object
+ * (represented by type T), along with a priority value.
+ */
+public class ArrayHeap<T> {
+	private ArrayList<Node> contents = new ArrayList<Node>();
+
+	/**
+	 * Inserts an item with the given priority value. This is enqueue, or offer.
+	 */
+	public void insert(T item, double priority) {
+
+	}
+
+	/**
+	 * Returns the Node with the smallest priority value, but does not remove it
+	 * from the heap.
+	 */
+	public Node peek() {
+		// TODO Complete this method!
+		return null;
+	}
+
+	/**
+	 * Returns the Node with the smallest priority value, and removes it from
+	 * the heap. This is dequeue, or poll.
+	 */
+	public Node removeMin() {
+		// TODO Complete this method!
+		return null;
+	}
+
+	/**
+	 * Change the node in this heap with the given item to have the given
+	 * priority. For this method only, you can assume the heap will not have two
+	 * nodes with the same item. Check for item equality with .equals(), not ==
+	 */
+	public void changePriority(T item, double priority) {
+		// TODO Complete this method!
+	}
+
+	/**
+	 * Prints out the heap sideways.
+	 */
+	@Override
+	public String toString() {
+		return toStringHelper(1, "");
+	}
+
+	/* Recursive helper method for toString. */
+	private String toStringHelper(int index, String soFar) {
+		if (getNode(index) == null) {
+			return "";
+		} else {
+			String toReturn = "";
+			int rightChild = getRightOf(index);
+			toReturn += toStringHelper(rightChild, "        " + soFar);
+			if (getNode(rightChild) != null) {
+				toReturn += soFar + "    /";
+			}
+			toReturn += "\n" + soFar + getNode(index) + "\n";
+			int leftChild = getLeftOf(index);
+			if (getNode(leftChild) != null) {
+				toReturn += soFar + "    \\";
+			}
+			toReturn += toStringHelper(leftChild, "        " + soFar);
+			return toReturn;
+		}
+	}
+
+	private Node getNode(int index) {
+		if (index >= contents.size()) {
+			return null;
+		} else {
+			return contents.get(index);
+		}
+	}
+
+	private void setNode(int index, Node n) {
+		// In the case that the ArrayList is not big enough
+		// add null elements until it is the right size
+		while (index + 1 >= contents.size()) {
+			contents.add(null);
+		}
+		contents.set(index, n);
+	}
+
+	/**
+	 * Swap the nodes at the two indices.
+	 */
+	private void swap(int index1, int index2) {
+		Node node1 = getNode(index1);
+		Node node2 = getNode(index2);
+		this.contents.set(index1, node2);
+		this.contents.set(index2, node1);
+	}
+
+	/**
+	 * Returns the index of the node to the left of the node at i.
+	 */
+	private int getLeftOf(int i) {
+		// TODO Complete this method!
+		return 0;
+	}
+
+	/**
+	 * Returns the index of the node to the right of the node at i.
+	 */
+	private int getRightOf(int i) {
+		// TODO Complete this method!
+		return 0;
+	}
+
+	/**
+	 * Returns the index of the node that is the parent of the node at i.
+	 */
+	private int getParentOf(int i) {
+		// TODO Complete this method!
+		return 0;
+	}
+
+	/**
+	 * Adds the given node as a left child of the node at the given index.
+	 */
+	private void setLeft(int index, Node n) {
+		// TODO Complete this method!
+	}
+
+	/**
+	 * Adds the given node as the right child of the node at the given index.
+	 */
+	private void setRight(int inde, Node n) {
+		// TODO Complete this method!
+	}
+
+	/**
+	 * Bubbles up the node currently at the given index.
+	 */
+	private void bubbleUp(int index) {
+		// TODO Complete this method!
+	}
+
+	/**
+	 * Bubbles down the node currently at the given index.
+	 */
+	private void bubbleDown(int inex) {
+		// TODO Complete this method!
+	}
+
+	/**
+	 * Returns the index of the node with smaller priority. Precondition: Not
+	 * both of the nodes are null.
+	 */
+	private int min(int index1, int index2) {
+		Node node1 = getNode(index1);
+		Node node2 = getNode(index2);
+		if (node1 == null) {
+			return index2;
+		} else if (node2 == null) {
+			return index1;
+		} else if (node1.myPriority < node2.myPriority) {
+			return index1;
+		} else {
+			return index2;
+		}
+	}
+
+	public class Node {
+		private T myItem;
+		private double myPriority;
+
+		private Node(T item, double priority) {
+			myItem = item;
+			myPriority = priority;
+		}
+
+		public T item() {
+			return myItem;
+		}
+
+		public double priority() {
+			return myPriority;
+		}
+
+		@Override
+		public String toString() {
+			return item().toString() + ", " + priority();
+		}
+	}
+
+	public static void main(String[] args) {
+		ArrayHeap<String> heap = new ArrayHeap<String>();
+		heap.insert("c", 3);
+		heap.insert("i", 9);
+		heap.insert("g", 7);
+		heap.insert("d", 4);
+		heap.insert("a", 1);
+		heap.insert("h", 8);
+		heap.insert("e", 5);
+		heap.insert("b", 2);
+		heap.insert("c", 3);
+		heap.insert("d", 4);
+		System.out.println(heap);
+	}
+
+}
---
+++ lab10/spec/lab10.md
@@ -0,0 +1,61 @@
+~ number: 10
+~ title: Priority Queues
+
+Representing a tree with an array
+-------
+
+You've seen two approaches to implementing a sequence data structure: either using an array, or using linked nodes. We extended our idea of linked nodes to implement a tree data structure. It turns out we can also use an array to represent a tree.
+
+Here's how we implement a binary tree: 
+
+- The root of the tree will be in position 1 of the array (nothing is at position 0). We can define the position of every other node in the tree recursively:
+
+-  The left child of a node at position `n` is at position `2n`. 
+
+- The right child of a node at position `n` is at position `2n + 1`. 
+
+- The parent of a node at position `n` is at position `n/2`.
+
+Work with binary heaps
+--------
+
+#### Binary Heaps Defined
+
+In this lab, you will be making a priority queue using a binary min heap (where smaller values correspond to higher priorities). Recall from lecture: Binary min heaps are basically just binary trees (but _not_ binary search trees) -- they have all of the same invariants of binary trees -- with two extra invariants:
+
+- __Invariant 1:__ the tree must be _maximally balanced_ (more on this later)
+
+- __Invariant 2:__ every node is smaller than its descendents (there is another variation called a binary _max_ heap where the reverse is true)
+
+Invariant 2 guarantees that the min element will always be at the root of the tree. This helps us access that item quickly, which is what we need for a priority queue. 
+
+We need to make sure binary heap methods maintain the above two invariants. Here's how we do it:
+
+__Add an item__
+
+1. Put the item you're adding in the next available spot in the bottom row
+
+2. Swap the item you just added with its parent until it is larger than its parent, or until it is the new root. This is called _bubbling up_.
+
+__Remove the min item__
+
+1. Replace the item at the root with the item in the last bottom spot
+
+2. _Bubble down_ the new root until it is smaller than both its children. If you reach a point where you can either bubble down through the left or right child, you must choose the smaller of the two.
+
+#### Maximally Balanced Trees
+
+There are a couple different notions of what it means for a tree to be well balanced. A binary heap must always be what is called _maximally balanced_ (also sometimes called _complete_).
+
+A maximally balanced tree:
+
+- Has all available positions for nodes filled, except for possibly the last row, which must be filled left-to-right
+
+#### Writing Heap Methods
+
+The class `ArrayHeap` implements a binary max heap using an `ArrayList` (just like `ArrayBST` implemented a BST). Fill in the missing methods in `ArrayHeap.java`.
+
+Respect the abstraction! -- `insert`, `removeMin`, and `changePriority` may use the methods `bubbleUp` and `bubbleDown`. `bubbleUp` and `bubbleDown` may use `getLeft`, `getRight`, and `getParent`. 
+
+
+
