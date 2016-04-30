702ccb88ca6670630db99f5c56440777f520ed15
Josh Hug <hug@cs.berkeley.edu>
2016-04-29 18:30:38 -0700

    hkn survey skeleton

---
+++ hwEC2/MagicWordEC2.java
@@ -0,0 +1,8 @@
+public class MagicWordEC2 {
+	public static String magicWord() {
+		/* replace magicWord with the magic word given to you when
+		 * you finished the survey. */
+		String magicWord = "";
+		return magicWord;
+	}
+} 
---
+++ hwEC2/readme
@@ -0,0 +1,3 @@
+If you went to lecture 1, use the lecture 1 magic word. 
+If you went to lecture 2, use the lecture 2 magic word. 
+If you went to the hkn office, use the hkn magic word.
